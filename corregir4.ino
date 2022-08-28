ac029a1e88fe0707e5209f547e8a2d674f4e7903
juanpanpanyz <101480015+juanpanpanyz@users.noreply.github.com>
2022-08-28 11:36:39 -0300

    Update corregir4.ino

--- corregir4.ino
+++ corregir4.ino
@@ -1,7 +1,12 @@
 #include <LibEstacionamiento.h>
+#include <Servo.h>
+Servo servo1;
+Servo servo2;
 
 void setup() {
     inicializar_sistema();
+    servo1.attach(9); // el que esta mas cerca del pulsador
+    servo2.attach(10); //  el que esta mas lejos del pulsador
 }
 
 void loop() {
@@ -9,19 +14,19 @@ void loop() {
     delay(5000);
     luz_auto1.apagar(ROJO);
 
-    subir_barrera_entrada();
+    servo1.write(90)();
     luz_auto1.encender(VERDE);
     delay(10000);
-    bajar_barrera_entrada();
+    servo1.write(0)();
     luz_auto1.apagar(VERDE);
 
     luz_auto4.encender(ROJO);
     delay(5000);
     luz_auto4.apagar(ROJO);    
 
-    subir_barrera_salida();
+    servo1.write(90)();
     luz_auto4.encender(VERDE);
     delay(10000);
-    bajar_barrera_entrada();
+    servo1.write(180)();
     luz_auto4.apagar(VERDE);    
 }

b75572842ad5a1888008b1758c6fab7096c75e3c
juanpanpanyz <101480015+juanpanpanyz@users.noreply.github.com>
2022-08-29 13:56:58 -0300

    Update and rename corregir7.ino to 7.ino

---
+++ 7.ino
@@ -0,0 +1,33 @@
+#include <LibEstacionamiento.h>
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+void setup()
+{
+    inicializar_sistema();
+    servo1.attach(9);
+    servo2.attach(10);
+    servo1.write(0);
+    servo2.write(180);
+}
+
+void loop()
+{
+
+  Serial.println(digitalRead(A2));
+    delay(1000);
+    if (digitalRead(A2) == HIGH)
+    {
+        servo1.write(0);
+        delay(10000);
+        servo1.write(90);
+    }
+
+    if (digitalRead(A3) == HIGH)
+    {
+        servo2.write(90);
+        delay(10000);
+        servo2.write(180);
+    }
+}

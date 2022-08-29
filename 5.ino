691051253618aa55cc819e199725c83b4eaec16e
Defe <81602924+Defeeeee@users.noreply.github.com>
2022-08-29 14:11:22 -0300

    Update 5.ino

--- 5.ino
+++ 5.ino
@@ -1,47 +1,48 @@
 #include <LibEstacionamiento.h>
-#include <Servo.h>
 
-Servo servo1;
-Servo servo2;
 int i;
-int ii;
 
 void setup()
 {
     inicializar_sistema();
-    servo1.attach(9);
-    servo2.attach(10);
-    servo1.write(0);
-    servo2.write(180);
 }
 
 void loop()
 {
     luz_auto1.encender(ROJO);
-        mostrar_cartel(digitalRead(A4));
-        delay(1000);
-    if (analogRead(A4) == HIGH && i < 2)
+
+    if (estado_pulsador == PRESIONADO && i < 2)
     {
-        mostrar_cartel("e");
         luz_auto1.encender(ROJO);
         delay(5000);
         luz_auto1.apagar(ROJO);
 
-        servo1.write(90);
+        subir_barrera_entrada();
         luz_auto1.encender(VERDE);
         delay(10000);
-        servo2.write(90);
+        bajar_barrera_entrada();
         luz_auto1.apagar(VERDE);
 
         luz_auto4.encender(ROJO);
         delay(5000);
         luz_auto4.apagar(ROJO);
 
-        servo1.write(0);
+        subir_barrera_salida();
         luz_auto4.encender(VERDE);
         delay(10000);
-        servo2.write(180);
+        bajar_barrera_salida();
         luz_auto4.apagar(VERDE);
 
         i = 3;
-    }}
+    }
+
+    for (i = 1; i != 11; ++i)
+    {
+        subir_barrera_entrada();
+        bajar_barrera_salida();
+        delay(1000);
+        bajar_barrera_entrada();
+        subir_barrera_salida();
+        delay(1000);
+    }
+}

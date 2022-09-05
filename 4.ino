2fe77504274e5b95bf421213a7a12dbdd822710b
Defe <81602924+Defeeeee@users.noreply.github.com>
2022-09-05 13:48:19 -0300

    Update 4.ino

--- 4.ino
+++ 4.ino
@@ -7,6 +7,8 @@ void setup() {
     inicializar_sistema();
     servo1.attach(9); // el que esta mas cerca del pulsador
     servo2.attach(10); //  el que esta mas lejos del pulsador
+    servo1.write(0);
+    servo2.write(0);
 }
 
 void loop() {
